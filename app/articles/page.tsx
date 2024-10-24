19edb6375a32fdd50110f06bf1aaf8809912e73b
Chan Nyein Tun <29679269+channyeintun@users.noreply.github.com>
2024-10-24 23:22:36 +0700

    fix: update fetching

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -3,25 +3,24 @@
 import { Article } from "@/services/articles/Article";
 import { getArticles } from "@/services/articles/getArticles";
 import { useEffect, useState } from "react";
-import Cookie from 'js-cookie';
 import { getGuestArticles } from "@/services/articles/getGuestArticles";
+import { useSession } from "@/hooks/useSession";
 
 export default function ArticlesPage() {
     const [data, setData] = useState<Article[] | undefined>();
+    const { token } = useSession();
+
     useEffect(() => {
-        const authCookie = Cookie.get('authCookie');
-        if (authCookie) {
-            const fetchData = async () => {
+        if (token) {
+            (async () => {
                 const response = await getArticles();
                 setData(response.data);
-            }
-            fetchData();
+            })();
         } else {
-            const fetchData = async () => {
+            (async () => {
                 const response = await getGuestArticles();
                 setData(response.data);
-            }
-            fetchData();
+            })();
         }
     }, []);
 
@@ -30,9 +29,9 @@ export default function ArticlesPage() {
             <main className="grid w-screen h-screen p-20">
                 <ul className="flex flex-col gap-4">
                     {data.map(article => (
-                        <article key={article.id}>
-                            <h1>{article.title}</h1>
-                            <img src={article.imageUrl} />
+                        <article key={article.id} lang="my" className="flex gap-3 border border-gray-500 w-fit">
+                            <h1 className="w-28 text-balance p-2">{article.title}</h1>
+                            <img src={article.imageUrl} width={200} height={200} className="aspect-square object-cover" />
                         </article>
                     ))}
                 </ul>
