a9229a311f1809b8813b59483fc3a7cdbfa83a54
Chan Nyein Tun <29679269+channyeintun@users.noreply.github.com>
2024-10-24 23:22:53 +0700

    refactor: update useSession hook

--- hooks/useSession.tsx
+++ hooks/useSession.tsx
@@ -8,35 +8,34 @@ import { useRouter } from 'next/navigation'
 const AUTH_COOKIE_NAME = 'authCookie'
 
 type SessionState = {
-  loading: boolean;
   token: string | null;
 }
 
-type Logout = () => void
+type Logout = ({ redirect, path }: { redirect: boolean; path: string; }) => void
 
 export function useSession(): SessionState & { logout: Logout } {
 
   const router = useRouter();
 
   const [sessionState, setSessionState] = useState<SessionState>({
-    loading: true,
-    token: null,
+    token: getToken(),
   })
 
-  useEffect(() => {
-    const authCookie = Cookies.get(AUTH_COOKIE_NAME);
-    let token;
-    if (authCookie) {
-      const jwt: JWT = JSON.parse(authCookie);
-      token = jwt.accessToken;
-    }
-    setSessionState({ loading: false, token: token || null })
-  }, [])
-
-  const logout = () => {
+  const logout = ({ redirect = true, path = '/' }) => {
     Cookies.remove(AUTH_COOKIE_NAME);
-    router.push('/');
+    setSessionState({ token: null });
+    redirect && router.push(path);
   }
 
   return { ...sessionState, logout }
+}
+
+function getToken() {
+  const authCookie = Cookies.get(AUTH_COOKIE_NAME);
+  let token = null;
+  if (authCookie) {
+    const jwt: JWT = JSON.parse(authCookie);
+    token = jwt.accessToken;
+  }
+  return token;
 }
